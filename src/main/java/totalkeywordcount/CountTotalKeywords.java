470a9c194cf99606ce722cfe7933da216889cef7
alberto64 <alberto.dejesus@upr.edu>
2018-10-17 10:46:26 -0400

    Uploading mp code

---
+++ src/main/java/totalkeywordcount/CountTotalKeywords.java
@@ -0,0 +1,38 @@
+package totalkeywordcount;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+/**
+ * Created by ubuntu on 2/6/17.
+ */
+public class CountTotalKeywords {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: CountTotalKeywords <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Configuration config = new Configuration();
+        Job job = Job.getInstance(config, "Count Tweets");
+        job.setJarByClass(CountTotalKeywords.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(CountTotalKeywordsMapper.class);
+        job.setReducerClass(CountTotalKeywordsReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
---
+++ src/main/java/tweetkeywordcount/CountKeywordsPerTweetMapper.java
@@ -0,0 +1,58 @@
+package tweetkeywordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+
+import java.io.IOException;
+
+
+/**
+ * Created by ubuntu on 2/6/17.
+ */
+public class CountKeywordsPerTweetMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String tweet = value.toString();
+        Status status = null;
+        try {
+            status = TwitterObjectFactory.createStatus(tweet);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        if(status == null) { return; }
+        String text = status.getText();
+        if(text == null) { return; }
+        String[] words = text.toLowerCase().replaceAll("[;'\".,?/:]", "").replace(",","").split(" ");
+
+        for(String w : words) {
+            if(w.equals("trump")) {
+                context.write(new Text(String.valueOf(status.getId())), new IntWritable(1));
+            }
+            if(w.equals("zika")) {
+                context.write(new Text(String.valueOf(status.getId())), new IntWritable(1));
+            }
+            if(w.equals("flu")) {
+                context.write(new Text(String.valueOf(status.getId())), new IntWritable(1));
+            }
+            if(w.equals("diarrhea")) {
+                context.write(new Text(String.valueOf(status.getId())), new IntWritable(1));
+            }
+            if(w.equals("ebola")) {
+                context.write(new Text(String.valueOf(status.getId())), new IntWritable(1));
+            }
+            if(w.equals("headache")) {
+                context.write(new Text(String.valueOf(status.getId())), new IntWritable(1));
+            }
+            if(w.equals("measles")) {
+                context.write(new Text(String.valueOf(status.getId())), new IntWritable(1));
+            }
+        }
+    }
+}
